add2c6e333e9d4081b0525f3f4d6783b07af71ad
Charles Inwald <cinwald@gmail.com>
2023-06-10 14:39:57 -0400

    Pagination for MovieList component

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,20 +1,52 @@
-import React from "react";
-import Movie from "../types/movie";
+import React, { useState } from 'react';
+import Movie from '../types/movie';
 
-const MovieList = (props: {movies: Movie[]}) => <div>
-            <ul>
-            {props.movies.map((movie) => (
-                <li key={movie.id}>
-                    <h2>{movie.title}</h2>
-                    {movie.genres.map((genre) => (
-                        <span key={genre}>{genre}{' '}</span>
-                    ))}
-                </li>
+const MovieList = (props: { movies: Movie[] }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const moviesPerPage = 10;
+
+  // Calculate the index of the first and last movie to display on the current page
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = props.movies.slice(indexOfFirstMovie, indexOfLastMovie);
+
+  // Change the current page
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      <ul>
+        {currentMovies.map((movie) => (
+          <li key={movie.id}>
+            <h2>{movie.title}</h2>
+            {movie.genres.map((genre) => (
+              <span key={genre}>{genre} </span>
             ))}
-            {
-                props.movies.length === 0 && <li>No movies found</li>
-            }
-        </ul>
-</div>
+          </li>
+        ))}
+        {props.movies.length === 0 && <li>No movies found</li>}
+      </ul>
+
+      {/* Pagination */}
+      {props.movies.length > moviesPerPage && (
+        <div>
+          {Array.from({ length: Math.ceil(props.movies.length / moviesPerPage) }).map(
+            (_, index) => (
+              <button
+                key={index}
+                onClick={() => handlePageChange(index + 1)}
+                disabled={currentPage === index + 1}
+              >
+                {index + 1}
+              </button>
+            )
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MovieList;
